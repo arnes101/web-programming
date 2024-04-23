666120e3753f58cbeeab2717efdae0dd37e745f5
Arnes <arnes.rastoder@stu.ibu.edu.ba>
2024-04-23 03:08:26 +0200

    Third Milestone

--- js/checkout.js
+++ js/checkout.js
@@ -13,3 +13,19 @@ $(document).ready(function () {
 function goBack() {
     window.location.href = 'index.html'; // Replace 'index.html' with the actual file path
 }
+
+const formEl = document.querySelector('.paymnform');
+formEl.addEventListener('submit', event => {
+    event.preventDefault();
+    const formData = new FormData(formEl);
+    const data = Object.fromEntries(formData);
+    fetch('https://reqres.in/api/users', {
+        method: 'Post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
+});
